1814de093e7936bb49e55430c887d2409c19a2f4
maksimussokol <maksimus@2074.ru>
2022-09-24 15:08:57 +0500

    Ex 3 works

---
+++ ex3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("enter the number");
+int x = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < x; i++)
+{
+    if (i == x)
+        Console.Write(i * i * i);
+    else
+        Console.Write(i * i * i + ",");
+}
